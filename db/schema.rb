eb2ede2353703df681e73177725174a8f1d810a3
Morgan Bell <mtbell98@gmail.com>
2022-11-28 14:12:18 -0600

    removed facilities from hostels table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_194505) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_201046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_194505) do
     t.string "city"
     t.string "address"
     t.string "description"
-    t.string "facilities"
     t.float "rating"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
