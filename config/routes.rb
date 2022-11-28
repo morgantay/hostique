1ba44aa5dc1800a8e035ebfbb6d9cbc84172b0bc
Morgan Bell <mtbell98@gmail.com>
2022-11-28 14:49:24 -0600

    added essential routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :hostels, only: %i[index show new create update destroy] do
+    resources :rooms, only: %i[create]
+  end
+  resources :rooms, only: %i[update destroy] do
+    resources :reservations, only: %i[create]
+  end
+  resources :users, only: %i[new create destroy]
+  resources :reservations, only: %i[index update destroy]
 end
