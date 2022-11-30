44112de896f87c2629c09b207fd3422e40772814
Morgan Bell <mtbell98@gmail.com>
2022-11-30 09:04:42 -0600

    added stimulus for geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_172748) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_032009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_172748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_hostels_on_user_id"
   end
 
